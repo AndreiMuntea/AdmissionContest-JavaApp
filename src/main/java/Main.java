90e3d98c2c9d4ce28b332a59a69f5717d1171b62
Muntea Andrei <munteaandrei17@gmail.com>
2017-01-21 00:12:18 +0200

    Finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,13 @@
 import Controller.CandidateController;
 import Controller.OptionController;
 import Controller.SectionController;
-import DatabaseManager.DatabaseManager;
-import DatabaseManager.TableManager.AbstractTableManager;
-import DatabaseManager.TableManager.CandidateTableManager;
-import DatabaseManager.TableManager.OptionTableManager;
-import DatabaseManager.TableManager.SectionTableManager;
-import Domain.Candidate;
-import Domain.Option;
-import Domain.Section;
 import GUI.GUI;
 import Helper.ConfigLoader.ConfigLoader;
-import Helper.ConfigLoader.DatabaseConfigLoader;
-import Repository.DatabaseRepository.DatabaseRepository;
-import Repository.IRepository;
-import Utils.Pair.Pair;
-import Validator.CandidateValidator;
-import Validator.IValidator;
-import Validator.OptionValidator;
-import Validator.SectionValidator;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import javax.xml.crypto.Data;
 import java.net.URLDecoder;
 
 /**
@@ -39,7 +22,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext(URLDecoder.decode(getClass().getResource("/config/classes.xml").toString(),"UTF-8"));
+        ApplicationContext context = new ClassPathXmlApplicationContext(URLDecoder.decode(getClass().getResource("/config/classes.xml").toString(), "UTF-8"));
 
         ConfigLoader config = (ConfigLoader) context.getBean("configLoader");
 
